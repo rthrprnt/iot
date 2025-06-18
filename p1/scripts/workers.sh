83f6a7f9b8df98975fccb822eb66419d548275c8
rthrprnt <arthur.pernot@gmail.com>
2025-06-18 14:23:43 +0200

    issue arm64 image

--- p1/scripts/workers.sh
+++ p1/scripts/workers.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Get the master node's IP from the arguments
-MASTER_IP=$1
+MASTER_IP="192.168.56.110"
 
 # Get the token from the shared folder
 TOKEN=$(cat /vagrant/token)
 
 # Install K3s agent (worker) and join the master node
-curl -sfL https://get.k3s.io | K3S_URL=https://$MASTER_IP:6443 K3S_TOKEN=$TOKEN sh -
+curl -sfL https://get.k3s.io | K3S_URL=https://$MASTER_IP:6443 K3S_TOKEN=$TOKEN INSTALL_K3S_EXEC='--flannel-iface=eth1' sh -
